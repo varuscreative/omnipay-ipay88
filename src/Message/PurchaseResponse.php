06dd6031652321531d5a46dae0a1c53ce1cb79fd
Oğuzhan Arnak <arnakoguzhan.7@gmail.com>
2021-06-24 16:55:10 +0300

    redirect response added

--- src/Message/PurchaseResponse.php
+++ src/Message/PurchaseResponse.php
@@ -4,8 +4,9 @@ namespace Omnipay\IPay88\Message;
 
 
 use Omnipay\Common\Message\AbstractResponse;
+use Omnipay\Common\Message\RedirectResponseInterface;
 
-class PurchaseResponse extends AbstractResponse
+class PurchaseResponse extends AbstractResponse implements RedirectResponseInterface
 {
     public function getTransactionId()
     {
